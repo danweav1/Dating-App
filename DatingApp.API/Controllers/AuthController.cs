4e590356d3f4f142b0391780773a8037afbdc1b2
danweaver12 <danweaver12@gmail.com>
2020-06-16 12:39:07 +0200

    added authentication

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API.Controllers
 {
@@ -11,16 +17,17 @@ namespace DatingApp.API.Controllers
   public class AuthController : ControllerBase
   {
     private readonly IAuthRepository _repo;
-    public AuthController(IAuthRepository repo)
+    private readonly IConfiguration _config;
+
+    public AuthController(IAuthRepository repo, IConfiguration config)
     {
+      _config = config;
       _repo = repo;
     }
 
     [HttpPost("register")]
     public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
     {
-      // validate request
-
       userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
       if (await _repo.UserExists(userForRegisterDto.Username))
@@ -36,6 +43,46 @@ namespace DatingApp.API.Controllers
       var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
       return StatusCode(201); // the location of where to get the newly created entity (the new user)
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+    {
+      // making sure we have a user and their username/password matches what is stored in the database for that particular user
+      var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+      if (userFromRepo == null)
+      {
+        return Unauthorized();
+      }
+
+      // Our token will contain two claims. 1 for the user id and another for the username
+      var claims = new[]
+      {
+        new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+        new Claim(ClaimTypes.Name, userFromRepo.Username)
+      };
+
+      var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+      var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+      // create the token
+      var tokenDescriptor = new SecurityTokenDescriptor
+      {
+        Subject = new ClaimsIdentity(claims),
+        Expires = DateTime.Now.AddDays(1),
+        SigningCredentials = creds
+      };
+
+      var tokenHandler = new JwtSecurityTokenHandler();
+
+      var token = tokenHandler.CreateToken(tokenDescriptor); // will contain our jwt token we want to return to clien
+
+      return Ok(new
+      {
+        token = tokenHandler.WriteToken(token) // write our token into response we're sending back to client
+      });
     }
   }
 }
