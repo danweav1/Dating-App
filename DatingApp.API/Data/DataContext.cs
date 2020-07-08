2bca5618f0d9d5c759b315caeb1b1e5b3a826284
danweaver12 <danweaver12@gmail.com>
2020-07-08 14:10:43 +0200

    end of section 8

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -7,6 +7,7 @@ namespace DatingApp.API.Data
   {
     public DbSet<Value> Values { get; set; } // Values is used to represent the table name
     public DbSet<User> Users { get; set; }
+    public DbSet<Photo> Photos { get; set; }
 
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
