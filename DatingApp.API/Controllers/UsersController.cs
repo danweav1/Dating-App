4ac5ba9aa5ecaf912462f66f1048340c6a4d9d74
danweaver12 <danweaver12@gmail.com>
2020-08-23 16:51:10 +0200

    Added policy based authorization

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -7,7 +7,6 @@ using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Helpers;
 using DatingApp.API.Models;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.API.Controllers
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -91,6 +91,13 @@ namespace DatingApp.API
                 };
             });
 
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                options.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+                options.AddPolicy("VipOnly", policy => policy.RequireRole("VIP"));
+            });
+
             // This means every user has to be authenticated to call any of the controller functions
             services.AddControllers(options =>
             {
